f80fb4f381a921ac5204c747c2714bec41aa5813
Eduardo Hertz <eduardohertz@gmail.com>
2015-07-31 12:45:50 -0300

    Fixes the authorization basic hash sent in the requests

--- spec/lib/myfinance/request_spec.rb
+++ spec/lib/myfinance/request_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 describe Myfinance::Request do
   describe "#run" do
-    subject { described_class.new({ method: 'post', params: {}, url: Myfinance.configuration.url, user_agent: 'My Test User-Agent', authorization_hash: 'my-auth-hash' }) }
+    subject { described_class.new({ method: 'post', params: {}, url: Myfinance.configuration.url, user_agent: 'My Test User-Agent', token: 'my-auth-hash' }) }
 
     it "does a request using Typhoeus::Request" do
-      expect(Typhoeus::Request).to receive(:new).with("https://sandbox.myfinance.com.br", { method: "post", params: {}, headers: { "Accept" => "application/json", "Content-Type" => "application/json", "User-Agent" => "My Test User-Agent", "Authorization" => "Basic my-auth-hash" }, accept_encoding: "gzip" }).and_return(double(run: true, response: true))
+      expect(Typhoeus::Request).to receive(:new).with("https://sandbox.myfinance.com.br", { method: "post", params: {}, headers: { "Accept" => "application/json", "Content-Type" => "application/json", "User-Agent" => "My Test User-Agent", "Authorization" => "Basic bXktYXV0aC1oYXNoOlg=" }, accept_encoding: "gzip" }).and_return(double(run: true, response: true))
       subject.run
     end
 
@@ -17,6 +17,12 @@ describe Myfinance::Request do
     it "invokes Typhoeus::Request#response" do
       allow_any_instance_of(Typhoeus::Request).to receive(:run)
       expect_any_instance_of(Typhoeus::Request).to receive(:response)
+      subject.run
+    end
+
+    it 'generates the base 64 of the user token' do
+      allow_any_instance_of(Typhoeus::Request).to receive(:run)
+      expect(Base64).to receive(:strict_encode64).with("my-auth-hash:X").and_call_original
       subject.run
     end
   end
