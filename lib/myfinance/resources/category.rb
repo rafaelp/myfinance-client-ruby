3b8962713b7bec4ec8da858a01de697e93769d87
ferreiraandy <andyferreira92@gmail.com>
2017-02-24 16:34:44 -0300

    Implements categories create endpoint

--- lib/myfinance/resources/category.rb
+++ lib/myfinance/resources/category.rb
@@ -1,6 +1,13 @@
 module Myfinance
   module Resources
     class Category < Base
+
+      def create(params)
+        http.post("/categories", body: { category: params }) do |response|
+          respond_with_object response, "category"
+        end
+      end
+      
     end
   end
 end
--- spec/lib/myfinance/resources/category_spec.rb
+++ spec/lib/myfinance/resources/category_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
-describe Myfinance::Resources::Category do
+describe Myfinance::Resources::Category, vcr: true do
+  let(:entity_klass) { Myfinance::Entities::Category }
 
+  describe "#create" do
+    let(:params) { { name: "Category 1" } }
+
+    context "with success" do
+      it "creates a category successfully" do
+        result = client.categories.create(params)
+
+        expect(result).to be_a(entity_klass)
+        expect(result.name).to eql("Category 1")
+      end
+
+    end
+
+    context "with error" do 
+      it "creates with error" do
+        expect{
+          client.categories.create({})
+        }.to raise_error(Myfinance::RequestError) do |error|
+          expect(error.code).to eql(422)
+        end
+      end
+    end
+  end
 end
