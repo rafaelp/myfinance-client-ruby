00c41c2a1c58b26dcbd5d0ef845162627184e129
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2017-06-05 11:58:27 -0300

    fixup! Fix Sale attributes

--- lib/myfinance/entities/sale_rule.rb
+++ lib/myfinance/entities/sale_rule.rb
@@ -1,12 +1,16 @@
 module Myfinance
   module Entities
     class SaleRule < Base
+      attribute :id
+      attribute :sale_account_id
       attribute :issuer
       attribute :payment_method
       attribute :fee_percentage
+      attribute :fixed_fee_amount
       attribute :days_to_liquidation
-      attribute :sale_account_id
-      attribute :week_day
+      attribute :weekdays
+      attribute :created_at
+      attribute :updated_at
     end
   end
 end
--- spec/lib/myfinance/entities/sale_account_spec.rb
+++ spec/lib/myfinance/entities/sale_account_spec.rb
@@ -2,9 +2,12 @@ require "spec_helper"
 
 describe Myfinance::Entities::SaleAccount do
   it_behaves_like "entity_attributes", %i[
-    provider
+    id
     name
-    description
     entity_id
+    description
+    provider
+    created_at
+    updated_at
   ]
 end
