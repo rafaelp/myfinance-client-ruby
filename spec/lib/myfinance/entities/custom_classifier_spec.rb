6da29a517667c8bd9fd51cd14106f332f0fb2223
Lucas Mansur <lucas.mansur2@gmail.com>
2017-07-18 17:25:41 -0300

    Silence hound

--- spec/lib/myfinance/entities/custom_classifier_spec.rb
+++ spec/lib/myfinance/entities/custom_classifier_spec.rb
@@ -3,5 +3,5 @@ require "spec_helper"
 describe Myfinance::Entities::CustomClassifier do
   subject { described_class.new({}) }
 
-  it_behaves_like "entity_attributes", [:account_id, :created_at, :id, :name, :updated_at]
+  it_behaves_like "entity_attributes", %i[account_id created_at id name updated_at]
 end
